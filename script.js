91318c54d7772f451a2a8493e80f59c9a6ba83c1
shemar boyd <shemarlion@gmail.com>
2022-12-30 13:08:59 -0500

    added comments

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+// getting the current day 
 var rightNow = dayjs().format('MMM DD, YYYY ');
 $('#currentDay').html(rightNow);
 
@@ -5,13 +8,13 @@ $('#currentDay').html(rightNow);
 
 $(document).ready(function() {
 
-
+// calling the classes
 var saveBtn = $('.btn');
-// var theTimeEl = $('.time-block')
 
 
 
-  
+
+//   save button feature
 saveBtn.on('click', function() {
   var saveWords = $(this).parent().attr('id');
   var time = $(this).siblings('.description').val();
@@ -19,14 +22,16 @@ saveBtn.on('click', function() {
   console.log(time);
   localStorage.setItem(saveWords, JSON.stringify(time));
 });
+// function for changing the time 
 function timeChanger () {
   var currentHour = dayjs().hour();
   console.log(currentHour)
- 
+//  looping through the time blocks
   $('.time-block').each(function(){
     var id = $(this).attr('id');
     var idNum = parseInt(id.split('-')[1]);
     var value = localStorage.getItem(id);
+    // if statment to get the right classes
     $(this).find('.description').val(value);
     switch(true) {
       case idNum < currentHour:
@@ -43,6 +48,7 @@ function timeChanger () {
   
   
   };
+//   calling the function 
   timeChanger();
 
   
