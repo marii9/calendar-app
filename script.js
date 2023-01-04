f5a096042698803eea56c4a6a0e07f4b8a30a72c
shemar boyd <shemarlion@gmail.com>
2023-01-04 18:42:41 -0500

    changed java

--- script.js
+++ script.js
@@ -1,56 +1,64 @@
-
-
-// getting the current day 
-var rightNow = dayjs().format('MMM DD, YYYY ');
+// loads the html and css
+ $(document).ready(function() 
+ {
+// shows the current day
+var rightNow = dayjs().format('MMM DD, YYYY');
 $('#currentDay').html(rightNow);
 
 
 
-$(document).ready(function() {
-
-// calling the classes
-var saveBtn = $('.btn');
 
 
 
-
-//   save button feature
-saveBtn.on('click', function() {
-  var saveWords = $(this).parent().attr('id');
+// saveBtn with a click listener and saves the input from the user
+$('.saveBtn').on('click', function() {
+  var saveWords = $(this).closest('.time-block').attr('id');
   var time = $(this).siblings('.description').val();
   console.log(saveWords);
   console.log(time);
   localStorage.setItem(saveWords, JSON.stringify(time));
 });
-// function for changing the time 
+// calls the time changer function 
+timeChanger();
+ });
+//  gets data from the localStorage for each hour
+ $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
 function timeChanger () {
-  var currentHour = dayjs().hour();
+    // gets the current number of hours using moment.js
+    var currentHour = moment().hour();
   console.log(currentHour)
-//  looping through the time blocks
+// loops through the timeblocks
   $('.time-block').each(function(){
-    var id = $(this).attr('id');
-    var idNum = parseInt(id.split('-')[1]);
-    var value = localStorage.getItem(id);
-    // if statment to get the right classes
-    $(this).find('.description').val(value);
-    switch(true) {
-      case idNum < currentHour:
+    console.log(this)
+    var idNum = parseInt($(this).attr("id").split("hour")[1]);
+
+     
+// checks if the time is in the past,current,or future and applies a class in statement
+    if (idNum < currentHour) {
         $(this).addClass('past');
-        break;
-      case idNum == currentHour:
+      } else if (idNum == currentHour) {
         $(this).addClass('present');
-        break;
-      default:
+      } else {
         $(this).addClass('future');
-        
-    
-  }});
-  
-  
-  };
-//   calling the function 
-  timeChanger();
+      }
+})};
 
+
+
+ 
+  
+
+  
   
   
   
@@ -61,4 +69,4 @@ function timeChanger () {
  
  
   
-});
+
